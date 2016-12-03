58821d52c54c85b0faf1d4fe6d4ea32fdf35504d
spirinvladimir <spirinvladimir@gmail.com>
2016-12-03 21:11:15 +0200

    recursion with str->array

--- main.clj
+++ main.clj
@@ -1,14 +1,12 @@
 (println
-  (let [s1 (clojure.string/split (read-line) #"")
-        s2 (clojure.string/split (read-line) #"")]
-      (loop [p s1
-             q s2
-             r ""]
-        (let [a (first p)
-              b (first q)]
-            (if (nil? a)
-                r
-                (recur
-                    (rest p)
-                    (rest q)
-                    (str r a b)))))))
+  (loop [p (clojure.string/split (read-line) #"")
+         q (clojure.string/split (read-line) #"")
+         r []]
+      (let [a (first p)
+            b (first q)]
+          (if (nil? a)
+            (reduce str r)
+            (recur
+              (rest p)
+              (rest q)
+              (conj r a b))))))
